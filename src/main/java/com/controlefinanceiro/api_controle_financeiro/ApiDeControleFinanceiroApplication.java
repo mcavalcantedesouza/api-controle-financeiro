0fefba9d034a779fb3b41af9c62ef226edfd71f6
Michel Cavalcante <michelc.desouza@gmail.com>
2025-06-29 16:14:16 -0300

    chore: bootstrap initial settings

---
+++ src/main/java/com/controlefinanceiro/api_controle_financeiro/ApiDeControleFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.controlefinanceiro.api_controle_financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeControleFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeControleFinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/controlefinanceiro/api_controle_financeiro/ApiDeControleFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controlefinanceiro.api_controle_financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeControleFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
