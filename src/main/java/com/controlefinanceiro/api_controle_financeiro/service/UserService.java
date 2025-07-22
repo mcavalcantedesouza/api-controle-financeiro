4259c4af3a2cc1babdc272c240574eafd8883848
Michel Cavalcante <michelc.desouza@gmail.com>
2025-07-21 21:54:35 -0300

    feat: user creation implemented

--- src/main/java/com/controlefinanceiro/api_controle_financeiro/service/UserService.java
+++ src/main/java/com/controlefinanceiro/api_controle_financeiro/service/UserService.java
@@ -3,10 +3,12 @@ package com.controlefinanceiro.api_controle_financeiro.service;
 import com.controlefinanceiro.api_controle_financeiro.dto.request.UserRequest;
 import com.controlefinanceiro.api_controle_financeiro.dto.response.UserResponse;
 import com.controlefinanceiro.api_controle_financeiro.entity.UserEntity;
+import com.controlefinanceiro.api_controle_financeiro.mapper.UserMapper;
 import com.controlefinanceiro.api_controle_financeiro.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
@@ -16,12 +18,24 @@ public class UserService {
 
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
+    private final UserMapper mapper;
 
-//    public UserResponse createUser(UserRequest request) {
-//
-//        if (repository.findByEmail(request.getEmail()).isPresent()) {
-//
-//            throw new RuntimeException("Email já cadastrado.");
-//        }
-//    }
+    @Transactional
+    public UserResponse createUser(UserRequest request) {
+
+        if (repository.findByEmail(request.getEmail()).isPresent()) {
+            throw new IllegalArgumentException("Email já cadastrado.");
+        }
+
+        UserEntity userEntity = mapper.userRequestToUserEntity(request);
+
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
+        userEntity.setHashPassword(encodedPassword);
+
+        userEntity.setInitialBalance(BigDecimal.ZERO);
+
+        UserEntity savedUser = repository.save(userEntity);
+
+        return mapper.userEntityToUserResponse(savedUser);
+    }
 }
